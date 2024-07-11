83a338f5beaeacf8d0346fb7cc5271a3a19a19a7
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-11 18:24:53 +0200

    app runner

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ import ptvsd
 app = Flask(__name__)
 ptvsd.enable_attach(address=('0.0.0.0', 5679))
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000)
