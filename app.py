9af7385d480b6b9ddfc67451e1049c0e6c5b9283
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-11 18:23:44 +0200

    cors

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+from flask import Flask
+from flask_cors import CORS
 import ptvsd
 
+app = Flask(__name__)
 ptvsd.enable_attach(address=('0.0.0.0', 5679))
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
