1a64db8e4c02bd400cb7f2560cfa0b3e5c72fc13
Egor Ivanov <egor.i.ivanov@gmail.com>
2024-07-11 18:25:57 +0200

    port number

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 ptvsd.enable_attach(address=('0.0.0.0', 5679))
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=8010)
