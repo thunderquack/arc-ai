c336866792a2a868a3b0aefb138e11c51a2b4d37
Egor Ivanov <egor.i.ivanov@gmail.com>
2025-02-12 17:59:56 +0100

    hugging face is removed

--- process_events.py
+++ process_events.py
@@ -1,12 +1,13 @@
 import redis
-from model import process_text, initialize_model
+#from model import process_text, initialize_model
 
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
 # Initialize Model (ensure it's only done once)
-model, tokenizer, tokenizer_stream = initialize_model()
+#model, tokenizer, tokenizer_stream = initialize_model()
 
 def process_text_event(task_id):
+    '''
     redis_client.set(f'task_{task_id}_status', 'in_progress', ex=3600)
     text = redis_client.get(f'task_{task_id}_process_text_ai')
     if text:
@@ -14,3 +15,4 @@ def process_text_event(task_id):
         processed_text = process_text(model, tokenizer, tokenizer_stream, text)
         redis_client.set(f'task_{task_id}_process_text_ai', processed_text, ex=3600)
         redis_client.set(f'task_{task_id}_status', 'processed', ex=3600)
+    '''
