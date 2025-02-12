48707a1602543f2de6dcca99286778836bf707fd
Egor Ivanov <egor.i.ivanov@gmail.com>
2025-02-12 20:12:54 +0100

    extra file

--- model.py
+++
@@ -1,7 +0,0 @@
-#import onnxruntime_genai as og
-
-def initialize_model():
-    return model, tokenizer, tokenizer_stream
-
-def process_text(model, tokenizer, tokenizer_stream, text):
-    return output_text
