6bf7ad86e9e8727eb7269fdf7d4f859aebec7b70
Egor Ivanov <egor.i.ivanov@gmail.com>
2025-02-12 20:10:36 +0100

    attachment

--- model.py
+++ model.py
@@ -1,35 +1,7 @@
-import onnxruntime_genai as og
-
-MODEL_PATH = '/models/Phi-3/cpu-int4-rtn-block-32-acc-level-4'
+#import onnxruntime_genai as og
 
 def initialize_model():
-    print("Loading model...")
-    model = og.Model(MODEL_PATH)
-    tokenizer = og.Tokenizer(model)
-    tokenizer_stream = tokenizer.create_stream()
-    print("Model loaded")
     return model, tokenizer, tokenizer_stream
 
 def process_text(model, tokenizer, tokenizer_stream, text):
-    search_options = {
-        'max_length': 1024 * 120,
-        'repetition_penalty': 1.2,
-    }
-    chat_template = '\n{input} \n'
-    prompt = f'{chat_template.format(input=text)}'
-    input_tokens = tokenizer.encode(prompt)
-    
-    params = og.GeneratorParams(model)
-    params.set_search_options(**search_options)
-    params.input_ids = input_tokens
-    generator = og.Generator(model, params)
-    
-    output_text = ""
-    while not generator.is_done():
-        generator.compute_logits()
-        generator.generate_next_token()
-        new_token = generator.get_next_tokens()[0]
-        output_text += tokenizer_stream.decode(new_token)
-    
-    del generator
     return output_text
--- process_events.py
+++ process_events.py
@@ -1,18 +1,19 @@
+import ollama
 import redis
-#from model import process_text, initialize_model
 
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)
 
-# Initialize Model (ensure it's only done once)
-#model, tokenizer, tokenizer_stream = initialize_model()
-
 def process_text_event(task_id):
-    '''
     redis_client.set(f'task_{task_id}_status', 'in_progress', ex=3600)
     text = redis_client.get(f'task_{task_id}_process_text_ai')
     if text:
         text = text.decode('utf-8')
-        processed_text = process_text(model, tokenizer, tokenizer_stream, text)
+        input = []
+        input.append({'role': 'user', 'content': text})
+        response = ollama.chat(
+            model='mistral-nemo',
+            messages=input
+        )
+        processed_text = response['message']['content']
         redis_client.set(f'task_{task_id}_process_text_ai', processed_text, ex=3600)
         redis_client.set(f'task_{task_id}_status', 'processed', ex=3600)
-    '''
